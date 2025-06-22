dbd2a49e06fcc6257cabb7e9525b1b0e05edb2eb
Kirill <kireeffkirill21@yandex.ru>
2025-06-22 18:23:49 +0300

    codstyle

--- src/main/java/ru/yandex/practicum/filmorate/dal/GenreRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/GenreRepository.java
@@ -4,8 +4,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.catsgram.dal.BaseRepository;
-import ru.yandex.practicum.filmorate.model.GenreWithId;
 import ru.yandex.practicum.filmorate.dto.dtoclasses.GenreWithIdAndName;
+import ru.yandex.practicum.filmorate.model.GenreWithId;
 
 import java.util.Collection;
 import java.util.List;
@@ -13,12 +13,12 @@ import java.util.Optional;
 
 @Repository
 public class GenreRepository extends BaseRepository<GenreWithIdAndName> {
-    private final static String FIND_BY_ID_QUERY = "SELECT id AS genre_id, name AS genre_name FROM genre WHERE id = ?";
-    private final static String FIND_ALL_QUERY = "SELECT id AS genre_id, name AS genre_name FROM genre ORDER BY id ASC";
-    private final static String INSERT_GENRE_FILM_QUERY = "INSERT INTO film_genre SET genre_id = ?, film_id = ?";
-    private final static String DELETE_GENRE_FILM_QUERY = "DELETE FROM film_genre WHERE film_id = ?";
-    private final static String FIND_ALL_GENRES_FILM_BY_ID_QUERY = "SELECT " +
-            "FILM_GENRE.GENRE_ID AS genre_id,"+
+    private static final String FIND_BY_ID_QUERY = "SELECT id AS genre_id, name AS genre_name FROM genre WHERE id = ?";
+    private static final String FIND_ALL_QUERY = "SELECT id AS genre_id, name AS genre_name FROM genre ORDER BY id ASC";
+    private static final String INSERT_GENRE_FILM_QUERY = "INSERT INTO film_genre SET genre_id = ?, film_id = ?";
+    private static final String DELETE_GENRE_FILM_QUERY = "DELETE FROM film_genre WHERE film_id = ?";
+    private static final String FIND_ALL_GENRES_FILM_BY_ID_QUERY = "SELECT " +
+            "FILM_GENRE.GENRE_ID AS genre_id," +
             " G.NAME AS genre_name FROM FILM_GENRE " +
             "LEFT JOIN GENRE AS G on FILM_GENRE.GENRE_ID = G.ID " +
             "WHERE FILM_ID = ?";
